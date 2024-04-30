3286bcd4b103e3260ab432e5b2e72d3928b239d3
HW5434 <l990628l@gmail.com>
2024-04-30 18:23:18 +0900

    QNA 토글 구현중

--- kh13b/src/components/Menu.js
+++ kh13b/src/components/Menu.js
@@ -14,15 +14,18 @@ function Menu() {
                 <div className="container-fluid">
                     {/* React에서는 페이지간 이동을 NavLink 태그로 한다 */}
                     <NavLink className="navbar-brand" to="/">
-                        <img src="http://via.placeholder.com/200x100?text=Final Project"/>
+                        {/* 로고 들어가야 할 자리 */}
+                        <img src="http://via.placeholder.com/200x100?text=Final Project" />
                     </NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarColor01">
+
+                    {/* 메뉴들과 로그인 상태 */}
+                    <div className="collapse navbar-collapse " id="navbarColor01">
                         <ul className="navbar-nav me-auto">
                             {/* 메뉴 1 */}
-                            <li className="nav-item dropdown ms-auto">
+                            <li className="nav-item dropdown p-3">
                                 <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">메뉴 1번</a>
                                 <div className="dropdown-menu">
                                     <NavLink className="dropdown-item" to="/test01">테스트 1번</NavLink>
@@ -30,9 +33,8 @@ function Menu() {
                                     <NavLink className="dropdown-item" to="/test03">테스트 3번</NavLink>
                                 </div>
                             </li>
-                            
                             {/* 메뉴 2 */}
-                            <li className="nav-item dropdown">
+                            <li className="nav-item dropdown p-3">
                                 <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">메뉴 2번</a>
                                 <div className="dropdown-menu">
                                     <NavLink className="dropdown-item" to="/test01">테스트 1번</NavLink>
@@ -40,9 +42,8 @@ function Menu() {
                                     <NavLink className="dropdown-item" to="/test03">테스트 3번</NavLink>
                                 </div>
                             </li>
-
                             {/* 메뉴 3 */}
-                            <li className="nav-item dropdown">
+                            <li className="nav-item dropdown p-3">
                                 <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">메뉴 3번</a>
                                 <div className="dropdown-menu">
                                     <NavLink className="dropdown-item" to="/test01">테스트 1번</NavLink>
@@ -50,16 +51,15 @@ function Menu() {
                                     <NavLink className="dropdown-item" to="/test03">테스트 3번</NavLink>
                                 </div>
                             </li>
-
                             {/* 고객센터 */}
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">메뉴 3번</a>
+                            <li className="nav-item dropdown p-3">
+                                <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">고객센터</a>
                                 <div className="dropdown-menu">
                                     <NavLink className="dropdown-item" to="/testqna">공지사항 연습</NavLink>
                                 </div>
                             </li>
                             {/* 좌석 */}
-                            <li className="nav-item dropdown">
+                            <li className="nav-item dropdown p-3">
                                 <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">좌석</a>
                                 <div className="dropdown-menu">
                                     <NavLink className="dropdown-item" to="/seat">좌석배치도</NavLink>
@@ -67,18 +67,17 @@ function Menu() {
                             </li>
                         </ul>
 
-                         {/* 이 부분을 로그인 여부에 따라 다르게 표시 */}
-                         <div className="d-flex">
-                         
-                                <span className="me-2">
-                                    로그인
-                                </span>
-                                /
-                                <span className="ms-2">
-                                   회원가입
-                                </span>
-                     
-                            </div>
+                    {/* 회원에 대한 Col */}
+                    <div className="d-flex">
+                        <span className="me-2">
+                            로그인
+                        </span>
+                        /
+                        <span className="ms-2">
+                            회원가입
+                        </span>
+                    </div>
+
                     </div>
                 </div>
             </nav>
--- kh13b/src/components/integrated/Qna.js
+++ kh13b/src/components/integrated/Qna.js
@@ -1,19 +1,19 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Jumbotron from '../Jumbotron';
 import axios from '../utils/CustomAxios';
 import { RiDeleteBack2Fill } from "react-icons/ri";
+import { Modal } from 'bootstrap';
+import { IoIosArrowDown } from "react-icons/io";
 
 function Qna() {
 
     //State
     const [qnas, setQnas] = useState([]);
-    //등록 State
     const [input, setInput] = useState({
-        qnaNo : "",
-        qnaTitle : "",
-        qnaContent : ""
+        qnaNo: "",
+        qnaTitle: "",
+        qnaContent: ""
     });
-
     //최소 1회
     useEffect(() => {
         loadData();
@@ -24,7 +24,6 @@ function Qna() {
         const resp = await axios.get("/qna/");
         setQnas(resp.data);
     }, [qnas]);
-
     //삭제
     const deleteQna = useCallback(async (target) => {
         const choice = window.confirm("정말 삭제하시겠습니까?");
@@ -33,43 +32,153 @@ function Qna() {
         const resp = await axios.delete("/qna/" + target.qnaNo);
         loadData();
     }, [qnas]);
-
     //등록
-    const saveInput = useCallback(() => {
-
+    const saveInput = useCallback(async () => {
+        const resp = await axios.post("/qna/", input);
+        loadData();
     }, [input])
 
+    const cancelInput = useCallback(() => {
+        const choice = window.confirm("작성을 취소하시겠습니까?");
+        if (choice === false) return;
+        clearInput();
+        closeModal();
+    }, [input])
+
+    const changeInput = useCallback((e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        });
+    }, [input]);
+
+    const clearInput = useCallback(() => {
+        setInput({
+            qnaNo: "",
+            qnaTitle: "",
+            qnaContent: ""
+        });
+    }, [input]);
+
+    const bsModal = useRef();
+
+    //Modal
+    const openModal = useCallback(() => {
+        const modal = new Modal(bsModal.current);
+        modal.show();
+    }, [bsModal]);
+    const closeModal = useCallback(() => {
+        const modal = Modal.getInstance(bsModal.current);
+        modal.hide();
+    }, [bsModal]);
+
+    //openToggle
+    const openToggle = useCallback(() => {
+        loadData();
+    }, [qnas]);
+
+    //화면 출력
     return (
         <>
-            <Jumbotron title="질문글 테스트" />
+            <Jumbotron title="질문글 테스트" content="글에 대한 내용을" />
 
+            {/* 목록 */}
             <div className='row-4'>
                 <div className='col'>
-                    <table className='table table-hover'>
+                    <table className='table table-hover text-center'>
+
                         <thead>
                             <tr>
-                                <th>번호</th>
+                                <th width="10%">번호</th>
                                 <th>글제목</th>
-                                <th>글 내용</th>
-                                <th>임시 삭제버튼</th>
+                                <th width="15%">삭제버튼</th>
+                                <th width="15%">토글버튼</th>
                             </tr>
                         </thead>
-                        
+
                         <tbody>
                             {qnas.map((qna) => (
                                 <tr key={qna.qnaNo}>
                                     <td>{qna.qnaNo}</td>
-                                    <td>{qna.qnaTitle}</td>
-                                    <td>{qna.qnaContent}</td>
-                                    <td>        
-                                        <RiDeleteBack2Fill className='bnt text-danger'/>
+                                    <td className='text-start'>{qna.qnaTitle}</td>
+                                    <td>
+                                        <RiDeleteBack2Fill className='bnt text-danger' onClick={e => deleteQna(qna)} />
                                     </td>
+                                    <td>
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            <IoIosArrowDown className='text-center' />
+                                        </button>
+                                    </td>
+                                    <div id="collapseOne" class="row accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                        <div class="col accordion-body">
+                                            {qna.qnaContent}
+                                        </div>
+                                    </div>
                                 </tr>
                             ))}
+
+
                         </tbody>
                     </table>
                 </div>
             </div>
+
+
+
+            {/* 추가 버튼 */}
+            <div className="row mt-4">
+                <div className="col text-end">
+                    <button className="btn btn-primary"
+                        onClick={e => openModal()}>
+                        신규 등록
+                    </button>
+                </div>
+            </div>
+
+            {/* Modal */}
+            <div ref={bsModal} className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="staticBackdropLabel">질문글 쓰기</h1>
+                            <button type="button" className="btn-close" aria-label="Close"
+                                onClick={e => cancelInput()}></button>
+                        </div>
+                        <div className="modal-body">
+                            {/* 등록 화면 */}
+                            <div className="row">
+                                <div className="col">
+                                    <label>글 제목</label>
+                                    <input type="text" name="qnaTitle"
+                                        value={input.qnaTitle}
+                                        onChange={e => changeInput(e)}
+                                        className="form-control" />
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label></label>
+                                    <textarea type="text" name="qnaContent"
+                                        value={input.qnaContent}
+                                        onChange={e => changeInput(e)}
+                                        className="form-control" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="modal-footer">
+                            <button className='btn btn-success me-2' onClick={e => saveInput()}>
+                                등록
+                            </button>
+                            <button className='btn btn-danger' onClick={e => cancelInput()}>
+                                취소
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 
