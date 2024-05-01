23e661b4fae3d3fa6ea6060b4f4aa9a2ee87946f
yellll1n <yr1230_1103@hanmail.net>
2024-05-01 13:15:24 +0900

    우편주소 api 구현

--- kh13b/src/components/member/SignUp.js
+++ kh13b/src/components/member/SignUp.js
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import DaumPost from "./DaumPost";
+import AddressSearch from "./AddressSearch";
 
 const SignUp = () => {
 
@@ -11,9 +11,9 @@ const SignUp = () => {
         memberName:"",
         memberId: "",
         memberPw: "",
+        memberBirth: "",
         memberContact: "",
         memberEmail: "",
-        memberBirth: "",
         memberPost: "",
         memberAddress1: "",
         memberAddress2: "",
@@ -23,53 +23,72 @@ const SignUp = () => {
     const [emailDomain, setEmailDomain] = useState('');
     const [emailType, setEmailType] = useState('');
 
+    const handleSignUpInputChange = (e) => {
+        const { name, value } = e.target;
+        setInput(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
     const handleEmailIdChange = (e) => {
         setEmailId(e.target.value);
-      };
+    };
     
-      const handleEmailTypeChange = (e) => {
+    const handleEmailTypeChange = (e) => {
         setEmailType(e.target.value);
         setEmailDomain(e.target.value);
-      };
-    
-      const handleEmailDomainChange = (e) => {
-        setEmailDomain(e.target.value);
-      };
+    };
 
-      const [addressObj, setAddressObj] = useState({ areaAddress: '', townAddress: '' });
-      const [locationObj, setLocationObj] = useState(null); // setLocationObj 추가
-      
+    const handleEmailDomainChange = (e) => {
+        setEmailDomain(e.target.value);
+    };
+
+
+
     //view
     return (
         <>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>이름</label>
-                    <input type="text" name="memberName" value={input.memberName}/>
+                    <input type="text" name="memberName" value={input.memberName} className="form-control" onChange={handleSignUpInputChange}/>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>아이디</label>
-                    <input type="text" name="memberId" value={input.memberId}/>
+                    <input type="text" name="memberId" value={input.memberId} className="form-control" onChange={handleSignUpInputChange}/>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>비밀번호</label>
-                    <input type="password" name="memberPw" value={input.memberPw}/>
+                    <input type="password" name="memberPw" value={input.memberPw} className="form-control" onChange={handleSignUpInputChange}/>
+                </div>
+            </div>
+            <div className='row mt-4'>
+                <div className='col'>
+                    <label>비밀번호 확인</label>
+                    <input type="password" name="memberPw" value={input.memberPw} className="form-control" onChange={handleSignUpInputChange}/>
+                </div>
+            </div>
+            <div className='row mt-4'>
+                <div className='col'>
+                    <label>생년월일</label>
+                    <input type="text" name="memberBirth" value={input.memberBirth} className="form-control" onChange={handleSignUpInputChange}/>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>연락처</label>
-                    <input type="tel" name="memberContact" value={input.memberContact}/>
+                    <input type="tel" name="memberContact" value={input.memberContact} className="form-control" onChange={handleSignUpInputChange}/>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>이메일</label>
-                    <input type="text" value={emailId} onChange={handleEmailIdChange}/>
+                    <input type="text" value={emailId} onChange={handleEmailIdChange} className="form-control"/>
                     <span>@</span>
                     <select name="menuType" value={emailType} onChange={handleEmailTypeChange}>
                         <option value="">직접입력</option>
@@ -82,7 +101,7 @@ const SignUp = () => {
                         <option value="outlook.com">outlook.com</option>
                     </select>
                     <input type="text" name="email" value={emailDomain} readOnly={emailType === '' ? false : true}
-                        onChange={handleEmailDomainChange}/>
+                        onChange={handleEmailDomainChange} className="form-control"/>
                     
                     <a href="#" className="me-2">이메일 중복확인</a>
                     <a href="#" className="me-2">이메일 인증</a>
@@ -94,27 +113,8 @@ const SignUp = () => {
             </div>
             <div className='row mt-4'>
                 <div className='col'>
-                    <label>생년월일</label>
-                    <input type="text" name="memberBirth" value={input.memberBirth}/>
-                </div>
-            </div>
-            <div className='row mt-4'>
-                <div className='col'>
-                    <label>우편번호</label>
-                    <input type="text" name="memberPost" value={input.memberPost} readOnly />
-                    <DaumPost setAddressObj={setAddressObj} setLocationObj={setLocationObj} />
-                </div>
-            </div>
-            <div className='row mt-4'>
-                <div className='col'>
-                    <label>기본주소</label>
-                    <input type="text" name="memberAddress1" value={addressObj.areaAddress} readOnly />
-                </div>
-            </div>
-            <div className='row mt-4'>
-                <div className='col'>
-                    <label>상세주소</label>
-                    <input type="text" name="memberAddress2" value={input.memberAddress2}/>
+                    <label>주소</label><br/>
+                    <AddressSearch/>
                 </div>
             </div>
 
