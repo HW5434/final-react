f6bc59d8a230295d5193b13ae5fb718ef2ca8182
yellll1n <yr1230_1103@hanmail.net>
2024-05-01 22:49:23 +0900

    fix: 유효성 체크 진행중

--- kh13b/src/components/member/SignUp.js
+++ kh13b/src/components/member/SignUp.js
@@ -2,23 +2,19 @@ import { useCallback, useState } from 'react';
 import AddressSearch from "./AddressSearch/AddressSearch";
 import axios from "../utils/CustomAxios";
 import Jumbotron from './../Jumbotron';
+import './Member.css';
 
 
 const SignUp = () => {
 
     //State
-    const [join, setJoin] = useState([]);
-
     const [input, setInput] = useState({
         memberName:"",
         memberId: "",
-        memberIdCheck:"",
         memberPw: "",
         memberPwCheck:"",
         memberBirth: "",
-        memberBirthCheck:"",
         memberContact: "",
-        memberContactCheck:"",
         memberEmail: "",
         memberPost: "",
         memberAddress1: "",
@@ -38,6 +34,15 @@ const SignUp = () => {
         memberAddress2: false,
     });
 
+    const [checkId, setCheckId] = useState({
+        flag: false,
+        message: "",
+    });
+    const [checkEmail, setCheckEmail] = useState({
+        flag: false,
+        message: "",
+    });
+
     //등록 입력값 변경
     const handleSignUpInputChange = (e) => {
         const { name, value } = e.target;
@@ -45,43 +50,40 @@ const SignUp = () => {
             ...prevState,
             [name]: value
         }));
-    }
-    //등록값 유효성 체크
-    const changeValid = (e) => {
-        const name = e.target.name;
+        
         if(name === 'memberName') {
             setInputValid({
                 ...inputValid,
-                memberName : input.memberName !== ''
+                memberName : value !== '' && value.length > 1
             });
         } else if(name === 'memberId') {
             const regex = /^[a-z][a-z0-9]{7,19}$/;
             setInputValid({
                 ...inputValid,
-                memberId : regex.test(input.memberId)
+                memberId : regex.test(value)
             });
         } else if(name === "memberPw") {
             const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$])[A-Za-z0-9!@#$]{8,16}$/;
             setInputValid({
                 ...inputValid,
-                memberPw : regex.test(input.memberPw)
+                memberPw : regex.test(value)
             });
         } else if(name === "memberPwCheck") {
             setInputValid({
                 ...inputValid,
-                memberPwCheck : input.memberPw === input.memberPwCheck
+                memberPwCheck : input.memberPw === value
             });
         } else if(name === "memberBirth") {
             const regex = /^(19[0-9]{2}|20[0-9]{2})-(02-(0[1-9]|1[0-9]|2[0-8])|(0[469]|11)-(0[1-9]|1[0-9]|2[0-9]|30)|(0[13578]|1[02])-(0[1-9]|1[0-9]|2[0-9]|3[01]))$/;
             setInputValid({
                 ...inputValid,
-                memberBirth : regex.test(input.memberBirth)
+                memberBirth : regex.test(value)
             });
         } else if(name === "memberContact") {
             const regex = /^010[1-9][0-9]{3}[0-9]{4}$/;
             setInputValid({
                 ...inputValid,
-                memberContact : regex.test(input.memberContact)
+                memberContact : regex.test(value)
             });
         }
     }
@@ -91,12 +93,46 @@ const SignUp = () => {
         const resp = await axios.post("/member/", input);
         clearInput();
     }, [input]);
+
     //등록취소
     const cancelInput = useCallback(()=> {
         const choice = window.confirm("작성 중인 내용이 모두 삭제되고 메인 페이지로 돌아갑니다. 진행하시겠습니까");
         if(choice === false) return;
         window.location.href = "/Home"; // 메인 페이지로 이동
     }, [input]);
+
+    //아이디 중복
+    const doubleCheckId = async (memberId) => {
+        const res = await axios.get(`/member/doubleCheckId/${memberId}`);
+        if(res.data) {
+            setCheckId({
+                flag: true,
+                message: "사용할 수 있는 아이디입니다."
+            });
+        } else {
+            setCheckId({
+                flag: false,
+                message: "다시 입력해"
+            });
+        }
+    }
+
+    //아이디 중복
+    const doubleCheckEmail = async (memberEmail) => {
+        alert(memberEmail)
+        const res = await axios.get(`/member/doubleCheckEmail/${memberEmail}`);
+        if(res.data) {
+            setCheckEmail({
+                flag: true,
+                message: "사용할 수 있는 이메일입니다."
+            });
+        } else {
+            setCheckEmail({
+                flag: false,
+                message: "다시 입력해"
+            });
+        }
+    }    
 
     //입력값초기화
     const clearInput = useCallback(()=> {
@@ -122,7 +158,7 @@ const SignUp = () => {
         setEmailId(e.target.value);
         setInput(prevState => ({
             ...prevState,
-            memberEmail: e.target.value + emailDomain
+            memberEmail: e.target.value + "@" + emailDomain
         }));
     };
     const handleEmailTypeChange = (e) => {
@@ -130,14 +166,14 @@ const SignUp = () => {
         setEmailDomain(e.target.value);
         setInput(prevState => ({
             ...prevState,
-            memberEmail: emailId + e.target.value
+            memberEmail: emailId + "@" + e.target.value
         }));        
     };
     const handleEmailDomainChange = (e) => {
         setEmailDomain(e.target.value);
         setInput(prevState => ({
             ...prevState,
-            memberEmail: emailId + e.target.value
+            memberEmail: emailId + "@" + e.target.value
         }));
     };
 
@@ -150,37 +186,56 @@ const SignUp = () => {
             <div className='row mt-4'>
                 <div className='col'>
                     <label>이름</label>
-                    <input type="text" name="memberName" value={input.memberName} className="form-control" onChange={handleSignUpInputChange}/>
+                    <input type="text" name="memberName" value={input.memberName} className="form-control"  onChange={handleSignUpInputChange} />
+                    <p className={inputValid.memberName ? "trueValid" : "falseValid"}>
+                        {input.memberName === "" ? "" : inputValid.memberName ? "이름 잘 입력했어" : "이름 입력해"}
+                    </p>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>아이디</label>
+                    <button href="#" className="me-2" onClick={() => doubleCheckId(input.memberId)}>아이디 중복확인</button>
+                    <p className={checkId.flag ? "trueValid" : "falseValid"}>
+                        {checkId.message === "" ? "" : checkId.message}
+                    </p>
                     <input type="text" name="memberId" value={input.memberId} className="form-control" onChange={handleSignUpInputChange}/>
+                    <p className={inputValid.memberId ? "trueValid" : "falseValid"}>
+                        {input.memberId === "" ? "" : inputValid.memberId ? "아이디 잘 입력했어" : "아이디 다시 입력해"}
+                    </p>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>비밀번호</label>
                     <input type="password" name="memberPw" value={input.memberPw} className="form-control" onChange={handleSignUpInputChange}/>
+                    <p className={inputValid.memberPw ? "trueValid" : "falseValid"}>
+                        {input.memberPw === "" ? "" : inputValid.memberPw ? "비밀번호 잘 입력했어" : "비밀번호 다시 입력해"}
+                    </p>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>비밀번호 확인</label>
-                    <input type="password" name="memberPwCheck" value={input.memberPw} className="form-control" onChange={handleSignUpInputChange}/>
+                    <input type="password" name="memberPwCheck" value={input.memberPwCheck} className="form-control" onChange={handleSignUpInputChange}/>
+                    <p className={inputValid.memberPwCheck ? "trueValid" : "falseValid"}>
+                        {input.memberPwCheck === "" ? "" : inputValid.memberPwCheck ? "비밀번호 일치해" : "비밀번호 일치하지않아"}
+                    </p>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>생년월일</label>
-                    <input type="text" name="memberBirth" value={input.memberBirth} className="form-control" onChange={handleSignUpInputChange}/>
+                    <input type="date" name="memberBirth" value={input.memberBirth} className="form-control" onChange={handleSignUpInputChange}/>
                 </div>
             </div>
             <div className='row mt-4'>
                 <div className='col'>
                     <label>연락처</label>
                     <input type="tel" name="memberContact" value={input.memberContact} className="form-control" onChange={handleSignUpInputChange}/>
+                    <p className={inputValid.memberContact ? "trueValid" : "falseValid"}>
+                        {input.memberContact === "" ? "" : inputValid.memberContact ? "연락처 잘 입력했어" : "연락처 입력해"}
+                    </p>
                 </div>
             </div>
             <div className='row mt-4'>
@@ -201,7 +256,10 @@ const SignUp = () => {
                     <input type="text" name="email" value={emailDomain} readOnly={emailType === '' ? false : true}
                         onChange={handleEmailDomainChange} className="form-control"/>
                     
-                    <a href="#" className="me-2">이메일 중복확인</a>
+                    <button className="me-2" onClick={() => doubleCheckEmail(input.memberEmail)}>이메일 중복확인</button>
+                    <p className={checkEmail.flag ? "trueValid" : "falseValid"}>
+                        {checkEmail.message === "" ? "" : checkEmail.message}
+                    </p>
                     <a href="#" className="me-2">이메일 인증</a>
                     <div className="notice">
                         ※ 일부 이메일(gmail.com, hotmail.com, live.co.kr, outlook.com, nate.com, dreamwiz.com, empal.com 등)은 <span className="color_red">답변 메일 수신</span>이 원활하지 않을 수 있습니다. <br />
