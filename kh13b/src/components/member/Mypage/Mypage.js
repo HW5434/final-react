35fc3259377384a8801745af59ee7b473bc8d170
yellll1n <yr1230_1103@hanmail.net>
2024-05-12 19:34:52 +0900

    버튼 타입 추가

--- kh13b/src/components/member/Mypage/Mypage.js
+++ kh13b/src/components/member/Mypage/Mypage.js
@@ -43,7 +43,7 @@ const Mypage = () => {
                     {layout === 'update' && (
                         <div>
                             수정화면
-                            <button className='btn btn-danger' onClick={() => setLayout('delete')}>회원탈퇴</button>
+                            <button type="button" className='btn btn-danger' onClick={() => setLayout('delete')}>회원탈퇴</button>
                         </div>
                     )}
                     {layout === 'delete' && (
