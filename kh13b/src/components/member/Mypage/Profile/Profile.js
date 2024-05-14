c48499f88d519358a25b6a7e511791da523e2f68
yellll1n <yr1230_1103@hanmail.net>
2024-05-15 00:01:33 +0900

    마이페이지 프로필 디자인 수정

--- kh13b/src/components/member/Mypage/Profile/Profile.js
+++ kh13b/src/components/member/Mypage/Profile/Profile.js
@@ -1,5 +1,7 @@
 import './Profile.css';
 import { Link } from 'react-router-dom';
+import { IoTicketOutline, IoInformationCircleOutline } from "react-icons/io5";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const Profile = ({ member, layoutChange }) => {
 
@@ -11,7 +13,7 @@ const Profile = ({ member, layoutChange }) => {
                         <div className='profile'>
                             <div className='profile-main'>
                                 <div className='main-rap'>
-                                    <div className='profile-name'>{member.memberName}</div>
+                                    <div className='profile-name'>{member.memberName} 님</div>
                                     <div className='profile-bottom'>
                                         <dl className='bottom-datarap'>
                                             <dt className='datarap-text'>DATA1</dt>
@@ -25,19 +27,23 @@ const Profile = ({ member, layoutChange }) => {
                             <div className='profile-sub'>
                                 <div className='sub-rap'>
                                     <Link className='sub-link' onClick={() => layoutChange('my')}>
-                                        <div className='link-icon'>아이콘1</div>
+                                        <div className='link-icon'>
+                                            <IoTicketOutline className='profile-icon'/>
+                                        </div>
                                         <div className='sub-text'>예매내역</div>
                                         <div className='sub-count'>0</div>
                                     </Link>
                                     <Link className='sub-link' onClick={() => layoutChange('update')}>
-                                        <div className='link-icon'>아이콘2</div>
-                                        <div className='sub-text'>수정</div>
-                                        <div className='sub-count'>0</div>
+                                        <div className='link-icon'>
+                                            <IoInformationCircleOutline className='profile-icon'/>
+                                        </div>
+                                        <div className='sub-text'>내정보</div>
                                     </Link>
                                     <Link className='sub-link' onClick={() => layoutChange('editPassword')}>
-                                        <div className='link-icon'>아이콘3</div>
+                                        <div className='link-icon'>
+                                            <RiLockPasswordLine className='profile-icon' />
+                                        </div>
                                         <div className='sub-text'>비밀번호변경</div>
-                                        <div className='sub-count'>0</div>
                                     </Link>
                                 </div>
                             </div>
